aa64e29700241710254f0ff2a4705e6ef8a32b49
Craig <craigred99@gmail.com>
2021-03-02 21:55:29 +0000

    Added register form validation

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <b-card>
-      <b-form @submit.prevent="register()" @reset="onReset()" v-if="show">
+      <b-form
+        @submit.prevent="register()"
+        @reset="onReset()"
+        v-if="show"
+        novalidate
+      >
+        <!-- Name -->
         <b-form-group id="name" label="Name:" label-for="name">
           <b-form-input
             id="name"
@@ -9,9 +15,24 @@
             type="text"
             placeholder="Enter name"
             required
-          ></b-form-input>
+            :class="{
+              'is-invalid': submitted && $v.form.name.$error,
+            }"
+          />
+          <!-- Error message for name -->
+          <span
+            v-if="submitted && !$v.form.name.required"
+            class="invalid-feedback"
+            >Name is required</span
+          >
+          <span
+            v-if="submitted && !$v.form.name.minLength"
+            class="invalid-feedback"
+            >Name must be {{ $v.form.name.$params.minLength.min }} characters.
+          </span>
         </b-form-group>
 
+        <!-- Email  -->
         <b-form-group id="email" label="Email address:" label-for="email">
           <b-form-input
             id="email"
@@ -19,21 +40,80 @@
             type="email"
             placeholder="Enter email"
             required
-          ></b-form-input>
+            :class="{
+              'is-invalid': (submitted && $v.form.email.$error) || errors.email,
+            }"
+          />
+          <!-- Error message for email -->
+          <span v-if="submitted && errors.email" class="invalid-feedback">{{
+            errors.email[0]
+          }}</span>
+          <span
+            v-if="submitted && !$v.form.email.required"
+            class="invalid-feedback"
+            >Email is required</span
+          >
+          <span
+            v-if="submitted && !$v.form.email.email"
+            class="invalid-feedback"
+            >Must be a valid email (example@gmail.com)</span
+          >
         </b-form-group>
 
+        <!-- Password -->
         <b-form-group id="password" label="Your Password:" label-for="password">
           <b-form-input
             id="password"
             type="password"
-            v-model="form.password"
+            v-model.trim="form.password"
             placeholder="Enter password"
             required
-          ></b-form-input>
+            :class="{
+              'is-invalid': submitted && $v.form.password.$error,
+            }"
+          />
+          <!-- Error message for password -->
+          <span
+            v-if="submitted && !$v.form.password.required"
+            class="invalid-feedback"
+            >Password is required</span
+          >
+          <span
+            v-if="submitted && !$v.form.password.minLength"
+            class="invalid-feedback"
+            >Password must be
+            {{ $v.form.password.$params.minLength.min }} characters.
+          </span>
+        </b-form-group>
+
+        <!-- REPEAT Password -->
+        <b-form-group
+          id="repeat_password"
+          label="Repeat Password:"
+          label-for="repeat_password"
+        >
+          <b-form-input
+            id="repeat_password"
+            type="password"
+            v-model.trim="form.repeatPassword"
+            placeholder="Repeat password"
+            required
+            :class="{
+              'is-invalid': submitted && $v.form.repeatPassword.$error,
+            }"
+          />
+          <!-- Error message for repeat password -->
+          <span
+            v-if="!$v.form.repeatPassword.sameAsPassword"
+            class="invalid-feedback"
+            >Passwords must be identical</span
+          >
         </b-form-group>
 
         <b-button type="submit" variant="primary">Register</b-button>
-        <b-button type="reset" variant="danger">Reset</b-button>
+        <b-button type="reset" variant="danger" @click="$v.$reset"
+          >Reset</b-button
+        >
       </b-form>
     </b-card>
   </div>
@@ -41,6 +121,7 @@
 
 <script>
 import axios from "axios";
+import { required, minLength, email, sameAs } from "vuelidate/lib/validators";
 
 export default {
   name: "LoginForm",
@@ -50,13 +131,33 @@ export default {
         name: "",
         email: "",
         password: "",
-        checked: [],
+        repeatPassword: "",
       },
+      errors: {},
       show: true,
+      submitted: false,
     };
+  },
+  validations: {
+    form: {
+      name: { required, minLength: minLength(3) },
+      email: { required, email },
+      password: { required, minLength: minLength(6) },
+      repeatPassword: {
+        sameAsPassword: sameAs("password"),
+      },
+    },
   },
   methods: {
     register() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      }
+      // else Post form
       axios
         .post("https://craig-college-api.herokuapp.com/api/register", {
           name: this.form.name,
@@ -71,12 +172,13 @@ export default {
           this.$emit("register");
         })
         .catch((error) => {
-          console.log(error);
-          console.log(error.response.data);
+          if (error.response.data) {
+            this.errors = error.response.data;
+            console.log(this.errors);
+          }
         });
     },
-    onReset(event) {
-      event.preventDefault();
+    onReset() {
       // Reset our form values
       this.form.email = "";
       this.form.password = "";
